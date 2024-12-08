987b6e868f3da6a471dc3c93a7be29817130bf28
Esger <esgerj@gmail.com>
2024-12-08 02:51:24 +0100

    header with select raster type

--- src/resources/elements/rasterizer.js
+++ src/resources/elements/rasterizer.js
@@ -1,9 +1,19 @@
-
+import { inject } from 'aurelia-dependency-injection';
+@inject(Element)
 export class RasterizerCustomElement {
-	constructor() {
+	constructor(element) {
+		this._element = element;
 		this.rasters = [
 			{ name: 'Dotted', value: 'dotted' },
 			{ name: 'Lined', value: 'lined' }
 		];
+	}
+
+	attached() {
+		this._rasterElement = this._element.querySelector('.target');
+	}
+
+	rasterChanged(value) {
+		this._rasterElement.classList.remove('lined', 'dotted').add(value);
 	}
 }
