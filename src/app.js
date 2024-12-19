57663289c1bd5e3d0f77f6b526b32f85b8fd7f64
Esger <esgerj@gmail.com>
2024-12-19 22:34:54 +0100

    clear image other

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ export class App {
 		this._eventAggregator = eventAggregator;
 	}
 	attached() {
-		this._mapSubscription = this._eventAggregator.subscribe('map-changed', map => this.map = map);
+		this._mapSubscription = this._eventAggregator.subscribe('map-changed', map => {
+			if (map == 'image')
+				this.fileUrl = undefined;
+			this.map = map;
+		});
 		this._grayscaleSubscription = this._eventAggregator.subscribe('grayscale-changed', grayscale => this.grayscale = grayscale);
 		this._rasterSubscription = this._eventAggregator.subscribe('raster-changed', raster => this.raster = raster);
 		this._sizeSubscription = this._eventAggregator.subscribe('size-changed', size => this.size = size);
@@ -14,7 +18,6 @@ export class App {
 		this._fileChangedSubscription = this._eventAggregator.subscribe('file-changed', file => {
 			const fileUrl = URL.createObjectURL(file);
 			this.fileUrl = fileUrl;
-			console.log(fileUrl);
 		});
 	}
 	detached() {
