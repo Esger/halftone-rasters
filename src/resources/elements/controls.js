3329d1b6c6d45411335bf79c9c0dd15113411235
Esger <esgerj@gmail.com>
2024-12-19 22:59:42 +0100

    raster angle via rotate

--- src/resources/elements/controls.js
+++ src/resources/elements/controls.js
@@ -14,14 +14,13 @@ export class Controls {
 		];
 		this.selectedMap = this.maps[0];
 		this.rasters = [
-			{ id: 0, value: 'dotted-x' },
-			{ id: 1, value: 'dotted' },
-			{ id: 2, value: 'lined' },
-			{ id: 3, value: 'radial' }
+			{ id: 0, value: 'dotted' },
+			{ id: 1, value: 'lined' },
+			{ id: 2, value: 'radial' }
 		];
 		this.selectedRaster = this.rasters[0];
 		this.size = 32;
-		this.angle = 0;
+		this.angle = -45;
 	}
 
 	attached() {
